e4fc0d65270fc26e556f1314635f9c2c4283a1da
Meisophant Company <meisophant.co@gmail.com>
2026-03-05 16:12:15 +0100

    Create particles-config.js

---
+++ js/particles-config.js
@@ -0,0 +1,13 @@
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80 },
+    color: { value: "#60a5fa" },
+    shape: { type: "circle" },
+    opacity: { value: 0.4 },
+    size: { value: 3 },
+    move: {
+      enable: true,
+      speed: 1.5
+    }
+  }
+});
